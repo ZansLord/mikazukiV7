46f343ca29de309c05a9dd7ec94ef434dc5ac91b
ZansLord <shikaizaidan@gmail.com>
2022-08-07 03:02:26 +0700

    This Is V1

---
+++ plugins/inf-sc.js
@@ -0,0 +1,10 @@
+let handler = async m => m.reply(`
+            kyahhh Dameeee Onichan><
+`.trim()) //
+
+handler.customPrefix = /^(.sc|.script|sc|script)$/i
+handler.command = new RegExp
+
+handler.limit = false
+handler.mods = false 
+module.exports = handler
