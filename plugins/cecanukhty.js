ba3fcf7f18a316fb4042a1406aa039cbe2a5a316
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2022-08-07 04:54:46 +0700

    update cecanukhty.js

--- plugins/cecanukhty.js
+++ plugins/cecanukhty.js
@@ -6,7 +6,7 @@ fetch('https://raw.githubusercontent.com/arivpn/dbase/master/kpop/ukhty.txt')
 let handler = async (m, { conn }) => {
     let img = zzz[Math.floor(Math.random() * zzz.length)]
     if (!img) throw img
-    await conn.sendFile(m.chat, img, '', '© nih Cecan Ukhtynya jangan lupa subscribe channel https://m.youtube.com/channel/UC0rPHvwr0sJtaccF8Mm1Xng 👍', m, 0, { thumbnail: await (await fetch(img)).buffer() })
+    await conn.sendFile(m.chat, img, '', '© nih Cecan Ukhtynya 👍', m, 0, { thumbnail: await (await fetch(img)).buffer() })
 }
 handler.help = ['cecanukhty']
 handler.tags = ['asupan']
