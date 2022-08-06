41cd6d64624369522fb392a180a7abdba4f4e454
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2022-08-07 04:53:14 +0700

    update cecanindo.js

--- plugins/cecanindo.js
+++ plugins/cecanindo.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/indonesia'), 'Nih Jgn lupa subscribe Channel ARIE TUBE https://m.youtube.com/channel/UC0rPHvwr0sJtaccF8Mm1Xng', wm, 'NEXT', '.cecanindo', m)
+    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/indonesia'), 'Nih', wm, 'NEXT', '.cecanindo', m)
 }
 
 handler.help = ['cecanindo']
