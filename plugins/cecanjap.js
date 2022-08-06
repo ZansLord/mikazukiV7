216d214f68a0bed7bb27efe72cf700c9f0c06046
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2022-08-07 04:53:38 +0700

    update cecanjap.js

--- plugins/cecanjap.js
+++ plugins/cecanjap.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/japan'), 'Nih Jgn lupa subscribe Channel ARIE TUBE https://m.youtube.com/channel/UC0rPHvwr0sJtaccF8Mm1Xng', wm, 'NEXT', '.cevanjapan', m)
+    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/japan'), 'Nih', wm, 'NEXT', '.cevanjapan', m)
 }
 
 handler.help = ['cecanjapan']
